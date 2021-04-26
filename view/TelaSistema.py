333a6c9901fc3f949e8115d2cf8bb9261e7f9684
Higor <higorpiresoliveira@gmail.com>
2021-04-26 15:47:55 -0300

    Cria tela de menu

--- view/TelaSistema.py
+++ view/TelaSistema.py
@@ -1,12 +1,40 @@
 from utils.Terminal import Terminal
 from view.AbstractTela import AbstractTela
 from messages.Sistema import mensagens as mensagens_sistema
+import PySimpleGUI as sg
 
 
 class TelaSistema(AbstractTela):
     def __init__(self, controlador):
         super().__init__(controlador)
 
+        # Tela
+        self.__window = None
+        self.init_components()
+
+    def init_components(self):
+        sg.ChangeLookAndFeel('Reddit')
+        lista_botoes = map(lambda message: [sg.Button(message, button_color='#52b1eb' if mensagens_sistema.get('menu_sair_sistema') != message else '#f03737', auto_size_button=False, size=(30, 2))], [
+            mensagens_sistema.get('menu_clientes'),
+            mensagens_sistema.get('menu_funcionarios'),
+            mensagens_sistema.get('menu_fornecedores'),
+            mensagens_sistema.get('menu_cat_produto'),
+            mensagens_sistema.get('menu_formas_pagamento'),
+            mensagens_sistema.get('menu_produtos'),
+            mensagens_sistema.get('menu_pedidos'),
+            mensagens_sistema.get('menu_sair_sistema')
+        ])
+
+        layout = [
+            [sg.Text('Selecione uma das opções abaixo..'), ],
+            lista_botoes
+        ]
+
+        self.__window = sg.Window(
+            'Sistema Supermacado 2.0').Layout(layout)
+
     def mostrar_opcoes(self, opcoes=[]):
-        Terminal.clear_all(self)
-        return super().mostrar_opcoes(mensagens_sistema.get('titulo_tela_opcoes_modulo'), opcoes)
+        self.__window.Read()
+
+        # Terminal.clear_all(self)
+        # return super().mostrar_opcoes(mensagens_sistema.get('titulo_tela_opcoes_modulo'), opcoes)
