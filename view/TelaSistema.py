ec538dd035fa90bfd811629d860fb7635301d452
Higor <higorpiresoliveira@gmail.com>
2021-05-05 10:37:33 -0300

    Corrige bug

--- view/TelaSistema.py
+++ view/TelaSistema.py
@@ -34,7 +34,6 @@ class TelaSistema(AbstractTela):
             'Sistema Supermacado 2.0').Layout(layout)
 
     def mostrar_opcoes(self, opcoes=[]):
-        self.init_components()
         botao, values = self.__window.Read()
         return botao
 
